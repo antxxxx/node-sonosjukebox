dea316cc0d6be3da14e365c80642e37593de4278
Anthony Brown <anthony@found-it.net>
2015-11-25 17:44:56 +0000

    added stubs for controller functions

---
+++ api/controllers/settings.js
@@ -0,0 +1,20 @@
+module.exports = {
+    getAllSettings: getAllSettings,
+    getSetting: getSetting,
+    updateSetting: updateSetting
+}
+
+function getAllSettings(req, resp) {
+    
+}
+
+
+function getSetting(req, resp) {
+    
+}
+
+
+function updateSetting(req, resp) {
+    
+}
+
---
+++ api/controllers/tracks.js
@@ -0,0 +1,24 @@
+module.exports = {
+    getAllTracks: getAllTracks,
+    getTrack: getTrack,
+    updateTrack: updateTrack,
+    playTrack: playTrack
+}
+
+function getAllTracks(req, resp) {
+    
+}
+
+
+function getTrack(req, resp) {
+    
+}
+
+
+function updateTrack(req, resp) {
+    
+}
+
+function playTrack(req, resp) {
+    
+}
