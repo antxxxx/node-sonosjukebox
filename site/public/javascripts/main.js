b91c3b4125292781359a058f1022579b8bd24936
Anthony Brown <anthony@found-it.net>
2015-11-26 18:21:15 +0000

    made updating sonos ip work

--- site/public/javascripts/main.js
+++ site/public/javascripts/main.js
@@ -5,9 +5,23 @@
 
 function GetConfig(setting, placeholderid) {
     $.get('/api/jukebox/settings/' + setting, function (data) {
-        $('#' + placeholderid).text(data.value);
+        $('#' + placeholderid).val(data.value);
     });
 }
 
-
+function SetConfig(setting, placeholderid) {
+    var value = $('#' + placeholderid).val();
+    var postData = {
+        "value": value
+    };
+    $.ajax({
+        url:'/api/jukebox/settings/' + setting,
+        type:"POST",
+        data:JSON.stringify(postData),
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        success: function(){
+        }
+    });
+}
 GetConfig("sonos", "sonosip");
