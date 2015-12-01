b8318f36dc21111b3f4a1e0860c31d359f69d991
Anthony Brown <anthony@found-it.net>
2015-12-01 12:54:41 +0000

    auto compact db

--- helpers/jukeboxDB.js
+++ helpers/jukeboxDB.js
@@ -2,7 +2,7 @@
 
 var Datastore = require('nedb')
 var jukeboxDB = new Datastore({ filename: './data/jukebox.db', autoload: true });
-  
+jukeboxDB.persistence.setAutocompactionInterval(60000);
 module.exports = {
     jukeboxDB: jukeboxDB
 }
